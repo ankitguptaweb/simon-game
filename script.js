b3b9ac45d84ce931a252d58ed69573d0dfbb1a8b
ankitguptaweb <ankitguptakhajuran@gmail.com>
2026-08-05 18:31:10 +0530

    revise

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let gameSeq = [];
 let userSeq = [];
 let highestScore = 0;
-
 let btns = ["yellow", "red", "purple", "green"];
 
 let started = false;
@@ -9,65 +8,53 @@ let level = 0;
 
 let h2 = document.querySelector("h2");
 
-// =====================
-// Start Game (PC + Mobile)
-// =====================
-function startGame() {
-  if (!started) {
+document.addEventListener("keypress", function () {
+  if (started == false) {
+    console.log("game is started");
     started = true;
+
     levelUp();
   }
+});
+
+function btnFlash(btn) {
+  btn.classList.add("flash");
+  setTimeout(function () {
+    btn.classList.remove("flash");
+  }, 250);
 }
 
-// PC
-document.addEventListener("keydown", startGame);
-
-// Mobile
-document.addEventListener("touchstart", startGame);
-
-// =====================
-// Flash Effects
-// =====================
 function gameFlash(btn) {
-  btn.classList.add("flash");
-  setTimeout(() => {
-    btn.classList.remove("flash");
+  btn.classList.add("userflash");
+  setTimeout(function () {
+    btn.classList.remove("userflash");
   }, 250);
 }
 
 function userFlash(btn) {
   btn.classList.add("userflash");
-  setTimeout(() => {
+  setTimeout(function () {
     btn.classList.remove("userflash");
   }, 250);
 }
 
-// =====================
-// Next Level
-// =====================
 function levelUp() {
   userSeq = [];
   level++;
-
   h2.innerText = `Level ${level}`;
 
   let randIdx = Math.floor(Math.random() * 4);
   let randColor = btns[randIdx];
   let randBtn = document.querySelector(`.${randColor}`);
-
   gameSeq.push(randColor);
-
   console.log(gameSeq);
-
   gameFlash(randBtn);
 }
 
-// =====================
-// Check Answer
-// =====================
 function checkAns(idx) {
+  // let idx = level - 1;
   if (userSeq[idx] === gameSeq[idx]) {
-    if (userSeq.length === gameSeq.length) {
+    if (userSeq.length == gameSeq.length) {
       setTimeout(levelUp, 1000);
     }
   } else {
@@ -75,52 +62,29 @@ function checkAns(idx) {
       highestScore = level;
     }
 
-    h2.innerText = `Game Over!
-Score: ${level}
-Highest Score: ${highestScore}
-
-Tap anywhere or press any key to restart`;
+    h2.innerText = `Game Over! Score: ${level} | Highest Score: ${highestScore}
+    Press any key to restart`;
 
     reset();
   }
 }
 
-// =====================
-// Button Click
-// =====================
 function btnPress() {
-
-  // If first tap starts the game
-  if (!started) {
-    startGame();
-    return;
-  }
-
+  // console.log(this);
   let btn = this;
-
   userFlash(btn);
 
-  let userColor = btn.getAttribute("id");
-
+  userColor = btn.getAttribute("id");
   userSeq.push(userColor);
-
-  console.log(userSeq);
 
   checkAns(userSeq.length - 1);
 }
 
-// =====================
-// Button Events
-// =====================
 let allBtns = document.querySelectorAll(".btn");
-
-for (let btn of allBtns) {
+for (btn of allBtns) {
   btn.addEventListener("click", btnPress);
 }
 
-// =====================
-// Reset Game
-// =====================
 function reset() {
   started = false;
   gameSeq = [];
